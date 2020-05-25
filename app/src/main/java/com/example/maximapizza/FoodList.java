69a8406e22bb2c14ad8cccaf90dc8e57f7a26f8a
Semyon Konev <semyoyon@gmail.com>
2020-05-25 22:01:00 +0500

    На страничку блюда добавлено: 1) Описание 2) Выбор количества 3) Название блюда 4) Так же кнопка корзины во всем приложении

--- app/src/main/java/com/example/maximapizza/FoodList.java
+++ app/src/main/java/com/example/maximapizza/FoodList.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -61,7 +62,10 @@ public class FoodList extends AppCompatActivity {
                 foodViewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        Toast.makeText(FoodList.this, ""+local.getName(), Toast.LENGTH_SHORT).show();
+                        //Создаем переходи на новую страницу
+                        Intent foodDetail = new Intent(FoodList.this,FoodDetail.class);
+                        foodDetail.putExtra("FoodId",adapter.getRef(position).getKey());
+                        startActivity(foodDetail);
                     }
                 });
             }
